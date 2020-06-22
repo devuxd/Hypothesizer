9cf4e5f11d65971b4721ea255388b988080918d0
Abdulaziz <alobodi1@hotmail.com>
2020-06-23 00:37:26 +0300

    we can log the react component code now

--- src/api/devtools.ts
+++ src/api/devtools.ts
@@ -1,6 +1,3 @@
-import { List } from "material-ui";
-
-
 
 
 let backgroundPageConnection: chrome.runtime.Port;
@@ -22,18 +19,21 @@ const sendMessageToBackground = (message: String) => {
     })
 }
 const getSourceCode = () => {
-    console.log(build_response({}))
+    // console.log(build_response({}))
     //below source code gets the compiled JS file with all scripts, not what we want
-    // chrome.devtools.inspectedWindow.getResources(e => e.filter(obj => {
-    //     if (!obj.url.includes("node_modules") && obj.url.includes("localhost"))
-    //         obj.getContent(e => console.log(e))
-    // }))
+    // This code only get the source code that we want :)
+    chrome.devtools.inspectedWindow.getResources(e => e.filter(obj => {
+        if (obj.url.includes("src") && obj.url.includes("localhost")) {
+            obj.getContent(e => console.log(e))
+            console.log(obj)
+        }
+    }))
 }
 
 // following code was used from https://github.com/tomimick/chrome-ext-view-src
 
-function build_response(request:any) {
-    let arr_html:any = [], arr_js:any = [], arr_css:any = [], temp_html:any = [];
+function build_response(request: any) {
+    let arr_html: any = [], arr_js: any = [], arr_css: any = [], temp_html: any = [];
 
     var is_initial = !request;
 
@@ -41,18 +41,20 @@ function build_response(request:any) {
         console.log("line 39")
         get_js(arr_js, arr_html, is_initial, true);
         console.log("line 41");
-        var response = {"js":arr_js, "css":arr_css, "html": temp_html};
+        var response = { "js": arr_js, "css": arr_css, "html": temp_html };
 
         // get html body + template scripts
         get_css(arr_css, is_initial);
 
-        response.html = [{"inline":get_dom(),
-            "count":document.getElementsByTagName('*').length}];
-        for (var i = 0; i < arr_html.length ; i++) {
+        response.html = [{
+            "inline": get_dom(),
+            "count": document.getElementsByTagName('*').length
+        }];
+        for (var i = 0; i < arr_html.length; i++) {
             response.html.push(arr_html[i]);
         }
     } catch (e) {
-        return {"err": ""+e};
+        return { "err": "" + e };
     }
 
     return response;
@@ -60,19 +62,19 @@ function build_response(request:any) {
 
 // get body as string
 function get_dom() {
-//    return document.documentElement.outerHTML;
+    //    return document.documentElement.outerHTML;
 
     // truncate long scripts+styles in HTML, they are listed separately
 
     var dupNode = <Element>document.documentElement.cloneNode(true);
 
-    function truncate(nodes:any) {
+    function truncate(nodes: any) {
         var MAXLEN = 400;
         var i, s;
-        for(i=0; i<nodes.length; i++){
+        for (i = 0; i < nodes.length; i++) {
             s = nodes[i].innerHTML;
             if (s && s.length > MAXLEN)
-                nodes[i].innerHTML = s.substr(0, MAXLEN) + " truncated "+s.length+"bytes...";
+                nodes[i].innerHTML = s.substr(0, MAXLEN) + " truncated " + s.length + "bytes...";
         }
     }
 
@@ -84,12 +86,12 @@ function get_dom() {
 
 // enumerate JS scripts in page
 // returns 2 arrays: js and html content
-function get_js(a:any, a_html:any, mark_initial:any, show_onclick:any) {
+function get_js(a: any, a_html: any, mark_initial: any, show_onclick: any) {
     var i, node;
 
     var nodes: HTMLCollectionOf<HTMLScriptElement> | NodeListOf<Element> = document.getElementsByTagName("script");
     console.log(nodes);
-    for(i=0; i<nodes.length; i++){
+    for (i = 0; i < nodes.length; i++) {
         node = nodes[i];
         if (!node.type || node.type === "text/javascript") {
             console.log("line 93");
@@ -98,26 +100,25 @@ function get_js(a:any, a_html:any, mark_initial:any, show_onclick:any) {
         else if (node.type === "text/template"
             || node.type === "text/x-template"
             || node.type === "text/html") {
-                console.log("line 99");
-                pick_node(node, a_html, mark_initial);
-            }
+            console.log("line 99");
+            pick_node(node, a_html, mark_initial);
+        }
     }
     // inline onclick-handlers
     if (show_onclick) {
         nodes = document.querySelectorAll("*");
-        for(i=0; i<nodes.length; i++){
+        for (i = 0; i < nodes.length; i++) {
             node = nodes[i];
             if (node.getAttribute("onclick")) {
-                var item: {[k: string]: any} | null = {};
+                var item: { [k: string]: any } | null = {};
                 item = pick_node(node, a, mark_initial);
                 var s = "/* " + node.tagName.toLowerCase();
                 if (node.id)
-                    s += "#"+node.id;
+                    s += "#" + node.id;
                 if (node.className)
-                    s += "."+node.className;
+                    s += "." + node.className;
                 s += ".onclick = */\n";
-                if(item !== null)
-                {
+                if (item !== null) {
                     item.inline = s + node.getAttribute("onclick");
                     item.src = null;
                     item.dynamic = false;
@@ -129,35 +130,35 @@ function get_js(a:any, a_html:any, mark_initial:any, show_onclick:any) {
 }
 
 // enumerate CSS in page
-function get_css(a:any, mark_initial:any) {
+function get_css(a: any, mark_initial: any) {
     var i;
 
-	var csslist = document.styleSheets;
-	for(i = 0; i < csslist.length; i++){
-		var css = csslist[i];
+    var csslist = document.styleSheets;
+    for (i = 0; i < csslist.length; i++) {
+        var css = csslist[i];
 
-		pick_node(css.ownerNode, a, mark_initial);
+        pick_node(css.ownerNode, a, mark_initial);
 
-		parse_cssrules(css, a, mark_initial, 0);
-	}
-
-/*
-    var styles = document.getElementsByTagName("link");
-    for(i=0; i<styles.length; i++){
-        node = styles[i];
-        if (node.rel === "stylesheet" || node.type === "text/css")
-            pick_node(node, a, mark_initial);
+        parse_cssrules(css, a, mark_initial, 0);
     }
-    styles = document.getElementsByTagName("style");
-    for(i=0; i<styles.length; i++){
-        node = styles[i];
-        pick_node(node, a, mark_initial);
-    }*/
+
+    /*
+        var styles = document.getElementsByTagName("link");
+        for(i=0; i<styles.length; i++){
+            node = styles[i];
+            if (node.rel === "stylesheet" || node.type === "text/css")
+                pick_node(node, a, mark_initial);
+        }
+        styles = document.getElementsByTagName("style");
+        for(i=0; i<styles.length; i++){
+            node = styles[i];
+            pick_node(node, a, mark_initial);
+        }*/
 }
 
 // parse "@import file.css" declarations in given css file
-function parse_cssrules(cssNode:any, a:any, mark_initial:any, depth:any) {
-	if (depth > 10 || !cssNode)
+function parse_cssrules(cssNode: any, a: any, mark_initial: any, depth: any) {
+    if (depth > 10 || !cssNode)
         return;
 
     try {
@@ -171,32 +172,32 @@ function parse_cssrules(cssNode:any, a:any, mark_initial:any, depth:any) {
     }
 
     if (rules) {
-        for (var i =0; i < rules.length; i++) {
+        for (var i = 0; i < rules.length; i++) {
             var rule = rules[i];
 
             if (rule instanceof CSSImportRule) {
                 var s = rule.styleSheet;
-                var item: {[k: string]: any} | null = pick_node({'href':s.href}, a, mark_initial);
-                if(item !== null) item.imported = true;
-                parse_cssrules(rule.styleSheet, a, mark_initial, depth+1);
+                var item: { [k: string]: any } | null = pick_node({ 'href': s.href }, a, mark_initial);
+                if (item !== null) item.imported = true;
+                parse_cssrules(rule.styleSheet, a, mark_initial, depth + 1);
             }
         }
     }
 }
 
 // picks element's src-url or inline content
-function pick_node(node:any, array:any, mark_initial:any) {
+function pick_node(node: any, array: any, mark_initial: any) {
     // skip extension scripts
     var src = node.href || node.src;
     // if (src && startsWith(""+src, "chrome-extension:"))
     //     return null;
 
-    var item: {[k: string]: any} = {};
+    var item: { [k: string]: any } = {};
 
     if (src)
-        item = {"src":src};
+        item = { "src": src };
     else
-        item = {"inline":node.innerText};
+        item = { "inline": node.innerText };
 
     // mark initially loaded elems
     if (mark_initial)
@@ -209,7 +210,7 @@ function pick_node(node:any, array:any, mark_initial:any) {
     return item;
 }
 
-function startsWith(s:any, sub:any) {
+function startsWith(s: any, sub: any) {
     return s.indexOf(sub) === 0;
 }
 
